da2d0f0572a2d4d6c06b87064eb6bd6a3f7b771a
Jacob McMichael <jacobmcmichael@gmail.com>
2022-03-21 18:02:43 -0700

    Created GET endpoints to retrieve category and cart data. Started implementing mutations for adding, updating, and deleting objects

--- data.js
+++ data.js
@@ -1,9 +1,70 @@
-import axios from "axios";
+import axios from 'axios';
 
+/**
+ * GET all products
+ */
 const GET_PRODUCTS = axios
-	.get('https://fakestoreapi.com/products')
-	.then((response) => {
-		return response.data;
-	});
+	.get(`https://fakestoreapi.com/products`)
+	.then((response) => response.data);
 
-export default GET_PRODUCTS;
+/**
+ * GET a product with the specified id
+ *
+ * @param {*} id
+ * @returns a product
+ */
+const GET_PRODUCT = (id) =>
+	axios
+		.get(`https://fakestoreapi.com/products/${id}`)
+		.then((response) => response.data);
+
+/**
+ *	GET a list of all categories
+ */
+const GET_CATEGORIES = axios
+	.get(`https://fakestoreapi.com/products/categories`)
+	.then((response) => response.data);
+
+/**
+ * GET products with the specified category
+ *
+ * @param {*} category
+ * @returns a list of products
+ */
+const GET_CATEGORY = (category) =>
+	axios
+		.get(`https://fakestoreapi.com/products/category/${category}`)
+		.then((response) => response.data);
+
+/**
+ * GET all users' shopping carts
+ */
+const GET_CARTS = axios
+	.get(`https://fakestoreapi.com/carts`)
+	.then((response) => response.data);
+
+/**
+ * GET a shopping cart with the specified user id
+ *
+ * @param {*} id
+ * @returns a user's shopping cart
+ */
+const GET_CART = (id) =>
+	axios
+		.get(`https://fakestoreapi.com/carts?userId=${id}`)
+		.then((response) => response.data);
+
+const ADD_PRODUCTS = (product) =>
+	axios
+		.post(`https://fakestoreapi.com/products`, product)
+		.then((response) => console.log(response.data));
+
+export {
+	GET_PRODUCTS,
+	GET_PRODUCT,
+	GET_CATEGORIES,
+	GET_CATEGORY,
+	GET_CARTS,
+	GET_CART,
+	ADD_PRODUCTS
+};
--- index.js
+++ index.js
@@ -1,33 +1,94 @@
 import { ApolloServer, gql } from 'apollo-server';
-import GET_PRODUCTS from './data.js';
+import {
+	GET_PRODUCTS,
+	GET_PRODUCT,
+	GET_CATEGORIES,
+	GET_CATEGORY,
+	GET_CARTS,
+	GET_CART,
+	ADD_PRODUCTS,
+} from './data.js';
 
 const typeDefs = gql`
+    # Types
 	type Query {
-		products: [Product]
+		products: [Product!]!
+		product(id: ID!): Product!
+		categories: [String!]!
+		category(category: String!): [Product!]!
+		carts: [Cart!]!
+		# cart:(id: ID!): [Products!]!
 	}
 
-    type Product {
-        id: ID
-        title: String
-        price: Float
-        description: String
-        category: String
-        image: String
-        rating: Rating
+    type Mutation {
+        addProduct(input: AddProduct!): Product
     }
 
-    type Rating {
-        rate: Float
-        count: Int
+	type Product {
+		id: ID!
+		title: String!
+		price: Float!
+		description: String!
+		category: String!
+		image: String!
+		rating: Rating!
+	}
+
+	type Rating {
+		rate: Float!
+		count: Int!
+	}
+
+	type Cart {
+		id: ID!
+		userId: ID!
+		date: String!
+		products: [CartProduct!]!
+	}
+
+	type CartProduct {
+		productId: ID!
+		quantity: Int!
+	}
+
+    # Inputs
+    input AddProduct {
+        title: String!
+        price: Float!
+        description: String!
+        image: String!
+        category: String!
     }
 `;
 
 const resolvers = {
-    Query: {
-        products: () => {
-            return GET_PRODUCTS;
+	Query: {
+		products: () => {
+			return GET_PRODUCTS;
+		},
+		product: (parent, args) => {
+			return GET_PRODUCT(args.id) ? GET_PRODUCT(args.id) : null;
+		},
+		categories: () => {
+			return GET_CATEGORIES;
+		},
+		category: (parent, args) => {
+			return GET_CATEGORY(args.category)
+				? GET_CATEGORY(args.category)
+				: null;
+		},
+		carts: () => {
+			return GET_CARTS;
+		},
+		// cart: (parent, args) => {
+		// 	return GET_CART(args.id) ? GET_CART(args.id) : null;
+		// },
+	},
+	Mutation: {
+		addProduct: (parent, args) => {
+            return ADD_PRODUCTS(args);
         }
-    },
+	},
 };
 
 const server = new ApolloServer({
